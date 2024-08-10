c10fcdcd134e483af564d89501917fd7ad4c3919
larads <dasilval.mariana@gmail.com>
2024-08-10 13:36:19 -0400

    🌈 Interface da aplicação - Separando componentes

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+    return (
+      <div className="flex flex-col gap-1">
+        <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
+        <span className="text-sm text-zinc-400">Agatha Desde Sempre | Trailer Oficial Dublado | Disney+</span>
+      </div>
+    );
+  }
