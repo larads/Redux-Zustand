970d13f1abff34c389aa7e170aac3111dabdcb8f
larads <dasilval.mariana@gmail.com>
2024-08-11 13:29:47 -0400

    🎉 init: Testes unitários no zustand

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,17 @@
+import { useCurrentLesson, useStore } from "../zustand-store";
+
 export function Header() {
-    return (
-      <div className="flex flex-col gap-1">
-        <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
-        <span className="text-sm text-zinc-400">Agatha Desde Sempre | Trailer Oficial Dublado | Disney+</span>
-      </div>
-    );
+  const { currentModule, currentLesson } = useCurrentLesson()
+  const isLoading = useStore(store => store.isLoading)
+
+  if (isLoading) {
+    return <h1 className="text-2xl font-bold">Carregando...</h1>
   }
+
+  return (
+    <div className="flex flex-col gap-1">
+      <h1 className="text-2xl font-bold">{currentLesson?.title}</h1>
+      <span className="text-sm text-zinc-400">Módulo "{currentModule?.title}"</span>
+    </div>
+  );
+}
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,14 +1,37 @@
 import ReactPlayer from 'react-player'
+import { Loader } from "lucide-react";
+import { useCurrentLesson, useStore } from "../zustand-store";
 
 export function Video() {
+  const { currentLesson } = useCurrentLesson()
+
+  const { isLoading, next } = useStore(store => {
+    return {
+      isLoading: store.isLoading,
+      next: store.next
+    }
+  })
+
+  function handlePlayNext() {
+    next()
+  }
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
-      <ReactPlayer
-        width="100%"
-        height="100%"
-        controls
-        url="https://www.youtube.com/watch?v=F1eIAdn9Cbg"
-      />
+      {isLoading ? (
+        <div className="flex h-full items-center justify-center">
+          <Loader className="w-6 h-6 text-zinc-400 animate-spin" />
+        </div>
+      ) : (
+        <ReactPlayer
+          width="100%"
+          height="100%"
+          controls
+          playing
+          onEnded={handlePlayNext}
+          url={`https://www.youtube.com/watch?v=${currentLesson?.id}`}
+        />
+      )}
     </div>
   );
 }
